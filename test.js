35bf5383128cf442ecfa7b8bc5a702ea52672d14
xahy123 <2985450893@qq.com>
2023-05-31 14:05:42 +0800

    主分支测试

--- test.js
+++ test.js
@@ -4,12 +4,6 @@ function wait(str) {
     sleep(1000)
     text(str).click()
   }
-  console.log('1');
-  console.log(2);
-  console.log(3);
-  console.log(4);
-  console.log(5);
-  console.log(6);
-  console.log(7);
-  console.log(8);
+  console.log(123);
+
 }
