d668bf410bd4e6c2ec866834857f3c2f7be8080f
xahy123 <2985450893@qq.com>
2023-05-31 14:14:34 +0800

    主分支测试3

--- qq.js
+++ qq.js
@@ -2,14 +2,14 @@ auto.waitFor()
 
 threads.start(function () {
   let startBtn = textContains('立即开始').findOne(2000);
-  sleep(500)
+  sleep(600)
   if (startBtn) {
     startBtn.click()
   }
 })
 //请求截图
 if (!requestScreenCapture()) {
-  toast("请求截图失败");
+  toast("请求截图失败，请检查后再试");
   exit();
 }
 
